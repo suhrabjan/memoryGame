138922327a3f9ef03a0cacd1f539f8ddcc17c548
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-10-10 02:30:35 -0500

    major updates to javascript and css files

--- js/js.js
+++ js/js.js
@@ -1,12 +1,12 @@
 const icons_arr = ['<i class="fa fa-diamond"></i>', '<i class="fa fa-diamond"></i>', '<i class="fa fa-paper-plane-o"></i>', '<i class="fa fa-paper-plane-o"></i>','<i class="fa fa-anchor"></i>', '<i class="fa fa-anchor"></i>', '<i class="fa fa-bolt"></i>', '<i class="fa fa-bolt"></i>', '<i class="fa fa-cube"></i>', '<i class="fa fa-cube"></i>', '<i class="fa fa-leaf"></i>', '<i class="fa fa-leaf"></i>', '<i class="fa fa-bicycle"></i>', '<i class="fa fa-bicycle"></i>', '<i class="fa fa-bomb"></i>', '<i class="fa fa-bomb"></i>'];
 const box = document.getElementById('box')
-let index;
-let targetSquare;
-const obj = new Square();
+const game = new Game();
+let temp;
+
 
 function Square() {
 
-    this.draw = function() {
+    this.draw = function(index, targetSquare) {
         if (!targetSquare.innerHTML) {
             targetSquare.innerHTML = icons_arr[index];
             targetSquare.firstChild.setAttribute('style', 'color: white; font-size: 35px; padding: 30% 0;')
@@ -14,40 +14,67 @@ function Square() {
         }
     }
 
-    this.animateIfFalse = function() {
-        targetSquare.animate([
-            { transform: 'translateX(0px)' },
-            { transform: 'translateX(10px)' },
-            { transform: 'translateX(-10px)' },
-            { transform: 'translateX(0px)' }
-            ],
-            {
-                duration: 200,
-                iterations: 3
-            });
+    this.animateIfFalse = function(targetSquare) {
+        targetSquare.style.animation = 'shake 0.9s ease';
     }
 
-    this.animateIfTrue = function() {
+    this.animateIfTrue = function(targetSquare) {
         targetSquare.style.backgroundColor = '#02ccba';
+        targetSquare.style.animation = 'confirm 0.8s ease';
+    }
 
+    this.clear = function(targetSquare) {
+        if (targetSquare.innerHTML) {
+            setTimeout(function(){
+                targetSquare.innerHTML = '';
+                targetSquare.setAttribute('style', '');
+            }, 900)
+        }
     }
 }
 
-//     this.clear = funciton() {
-
-//     }
-// }
 
 
-// function Game(object) {
+function Game(object) {
 
-//     this.shuffle = function() {
+    this.counter = 0;
+    this.obj_container = [];
 
-//     }
+    this.shuffle = function() {
 
-//     this.main = function() {
+    }
 
-//     }
+    this.start = function() {
+        for (let i = 0; i < icons_arr.length; ++i) {
+            const obj = new Square;
+            this.obj_container.push(obj);
+        }
+    }
+
+    this.main = function(idx, target) {
+        if (!target.innerHTML) {
+            this.counter += 1;
+        }
+        if (this.counter === 1) {
+            this.obj_container[idx].draw(idx, target);
+            temp1 = idx;
+            temp2 = target;
+        }
+        else if (this.counter === 2) {
+            this.counter = 0;
+            this.obj_container[idx].draw(idx, target);
+            if (icons_arr[temp1] === icons_arr[idx]) {
+                this.obj_container[temp1].animateIfTrue(temp2);
+                this.obj_container[idx].animateIfTrue(target);
+            }
+            else if (icons_arr[temp1] !== icons_arr[idx]) {
+                this.obj_container[temp1].animateIfFalse(temp2);
+                this.obj_container[idx].animateIfFalse(target);
+                this.obj_container[temp1].clear(temp2);
+                this.obj_container[temp1].clear(target);
+            }
+        }
+    }
 
 //     this.timer = function() {
 
@@ -65,18 +92,16 @@ function Square() {
 
 //     }
 
-// }
+}
 
-
+game.start();
+// console.log(game.obj_container[0] === game.obj_container[0]);
 
 box.addEventListener('click', function(e) {
+    e.preventDefault();
     if (e.target.classList[0] === 'icons') {
-        index = e.target.classList[1] - 1;
-        targetSquare = e.target;
-        obj.draw();
-        obj.animateIfTrue();
+        let index = e.target.classList[1] - 1;
+        let targetSquare = e.target;
+        game.main(index, targetSquare);
     }
-    // e.target.innerHTML = '<i class="fa fa-paper-plane-o"></i>';
-    // e.target.setAttribute('style', 'background: #02b3e4; font-size: 35px; color: white; text-align: center;')
-    // console.log(e);
 })
